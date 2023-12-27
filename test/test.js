920bbc4f68206e5df86cf61bcad98fd430b6e453
Yelias Ahmed <yeliasahmed89@gmail.com>
2023-12-27 21:22:11 +0600

    updated

--- test/test.js
+++ test/test.js
@@ -2,51 +2,122 @@ require("chromedriver");
 const assert = require("assert");
 const { Builder, By, Key, until } = require("selenium-webdriver");
 
-// async function test() {
-//   let driver = await new Builder().forBrowser("chrome").build();
+async function test() {
+  let driver = await new Builder().forBrowser("chrome").build();
 
-//   await driver.manage().window().maximize();
-//   await driver.get("https://google.com");
+ 
+  
 
-//   await driver.findElement(By.name("q")).click();
-//   await driver.findElement(By.name("q")).sendKeys("Dhaka", Key.RETURN);
+  try {
 
-//   console.log("Passed");
-//   await driver.quit();
-// }
-// test();
+const username = "//input[@id='username']";
+const password = "//input[@id='password']";
+const login = "//input[@id='Login']";
+const quicksetup = "(//*[name()='svg'][@data-key='setup'])[1]";
+const advancesetup = "//button[normalize-space()='Open Advanced Setup']";
+const users = "//div[@title='Users']//lightning-primitive-icon[@variant='bare']";
+const profile = "//a[normalize-space()='Profiles']";
+const sysAdmin = "//a[normalize-space()='System Administrator']";
 
-describe("Searching for a country and her capital", function () {
-  let driver;
-
-  before(async function () {
     
-    driver = await new Builder().forBrowser("chrome").build();
-  });
+  const url = "https://data-data-3699.lightning.force.com/lightning/setup/Profiles/home";
+  const url1 = "https://data-data-3699.my.salesforce.com/";
+  await driver.manage().setTimeouts({ implicit: 20000 });
+  await driver.manage().window().maximize();
 
-  it("Search on Google a Country", async function () {
-    await driver.manage().window().maximize();
-    await driver.get("https://google.com");
-    await driver.findElement(By.name("q")).click();
-    await driver.findElement(By.name("q")).sendKeys("Bangladesh", Key.RETURN);
+  await driver.get(url);
+
+
+  
+  await driver.findElement(By.xpath(username)).sendKeys("nightmarearmy420@gmail.com");
+  await driver.findElement(By.xpath(password)).sendKeys("Abcd1234");
+  await driver.findElement(By.xpath(login)).click();
+  // await new Promise(resolve => setTimeout(resolve, 8000));
+
+  
+
+  // await driver.findElement(By.xpath(quicksetup)).click();
+  // await driver.findElement(By.xpath(advancesetup)).click();
+  // const handles = await driver.getAllWindowHandles();
+  // await driver.switchTo().window(handles[1]);
+  // // await new Promise(resolve => setTimeout(resolve, 5000));
+  // await driver.findElement(By.xpath(users)).click();
+  await driver.findElement(By.xpath(profile)).click();
+  await new Promise(resolve => setTimeout(resolve, 6000));
+  console.log("Before scroll");
+  await driver.switchTo().frame(0);
+  await driver.executeScript('arguments[0].scrollIntoView();', await driver.findElement(By.xpath(sysAdmin)));
+  console.log("After scroll");
+  await driver.findElement(By.xpath(sysAdmin)).click();
+  console.log("After click on the system administrator");
+
+  } finally{
+
+    // await driver.quit();
+  }
+
+
+  console.log("Passed");
+  
+}
+test();
+
+// const username = "//input[@id='username']";
+// const password = "//input[@id='password']";
+// const login = "//input[@id='Login']";
+// const quicksetup = "(//*[name()='svg'][@data-key='setup'])[1]";
+// const advancesetup = "//button[normalize-space()='Open Advanced Setup']";
+// const users = "//div[@title='Users']//lightning-primitive-icon[@variant='bare']";
+// const profile = "//a[normalize-space()='Profiles']";
+// const sysAdmin = "//a[normalize-space()='System Administrator']";
+
+// describe("Salesforce functionality", function () {
+//   let driver;
+
+//   before(async function () {
     
-    await driver.wait(until.elementLocated(By.id("rcnt")), 10000);
-    await driver.findElement(By.name("q")).clear();
+//     driver = await new Builder().forBrowser("chrome").build();
+//   });
 
-    let title = await driver.getTitle();
-    assert.equal(title, "Bangladesh - Google Search");
-  });
+//   it("Clicked on the system administrator successfully", async function () {
+//     const url = "https://data-data-3699.my.salesforce.com/";
+//     await driver.manage().setTimeouts({ implicit: 20000 });
+//     await driver.manage().window().maximize();
+  
+//     await driver.get(url);
+  
+  
+    
+//     await driver.findElement(By.xpath(username)).sendKeys("nightmarearmy420@gmail.com");
+//     await driver.findElement(By.xpath(password)).sendKeys("Abcd1234");
+//     await driver.findElement(By.xpath(login)).click();
+//     // await new Promise(resolve => setTimeout(resolve, 8000));
+  
+    
+  
+//     await driver.findElement(By.xpath(quicksetup)).click();
+//     await driver.findElement(By.xpath(advancesetup)).click();
+//     const handles = await driver.getAllWindowHandles();
+//     await driver.switchTo().window(handles[1]);
+//     // await new Promise(resolve => setTimeout(resolve, 5000));
+//     await driver.findElement(By.xpath(users)).click();
+//     await driver.findElement(By.xpath(profile)).click();
+//     await driver.executeScript('arguments[0].scrollIntoView();', await driver.findElement(By.xpath(sysAdmin)));
+//     await driver.findElement(By.xpath(sysAdmin)).click();
 
-  it("Search on Google a city", async function () {
-    await driver.get("https://google.com");
-    await driver.findElement(By.name("q")).click();
-    await driver.findElement(By.name("q")).sendKeys("Dhaka", Key.RETURN);
-    await driver.wait(until.elementLocated(By.id("rcnt")), 5000);
-    await driver.findElement(By.name("q")).clear();
+//     console.log("Passed");
+//   });
 
-    let title = await driver.getTitle();
-    assert.equal(title, "Dhaka - Google Search");
-  });
+//   // it("Search on Google a city", async function () {
+//   //   await driver.get("https://google.com");
+//   //   await driver.findElement(By.name("q")).click();
+//   //   await driver.findElement(By.name("q")).sendKeys("Dhaka", Key.RETURN);
+//   //   await driver.wait(until.elementLocated(By.id("rcnt")), 5000);
+//   //   await driver.findElement(By.name("q")).clear();
 
-  after(() => driver && driver.quit());
-});
+//   //   let title = await driver.getTitle();
+//   //   assert.equal(title, "Dhaka - Google Search");
+//   // });
+
+//   // after(() => driver && driver.quit());
+// });
--- test/test2.js
+++ test/test2.js
@@ -1,52 +1,52 @@
-require("chromedriver");
-const assert = require("assert");
-const { Builder, By, Key, until, Browser } = require("selenium-webdriver");
+// require("chromedriver");
+// const assert = require("assert");
+// const { Builder, By, Key, until, Browser } = require("selenium-webdriver");
 
-// async function test() {
-//   let driver = await new Builder().forBrowser("chrome").build();
+// // async function test() {
+// //   let driver = await new Builder().forBrowser("chrome").build();
 
-//   await driver.manage().window().maximize();
-//   await driver.get("https://google.com");
+// //   await driver.manage().window().maximize();
+// //   await driver.get("https://google.com");
 
-//   await driver.findElement(By.name("q")).click();
-//   await driver.findElement(By.name("q")).sendKeys("Dhaka", Key.RETURN);
+// //   await driver.findElement(By.name("q")).click();
+// //   await driver.findElement(By.name("q")).sendKeys("Dhaka", Key.RETURN);
 
-//   console.log("Passed");
-//   await driver.quit();
-// }
-// test();
+// //   console.log("Passed");
+// //   await driver.quit();
+// // }
+// // test();
 
-describe("Searching for a country and her capital", function () {
-  let driver;
+// describe("Searching for a country and her capital", function () {
+//   let driver;
 
-  before(async function () {
+//   before(async function () {
     
-    driver = await new Builder().forBrowser("chrome").build();
-  });
+//     driver = await new Builder().forBrowser("chrome").build();
+//   });
 
-  it("Search on Google a Country", async function () {
-    await driver.manage().window().maximize();
-    await driver.get("https://google.com");
-    await driver.findElement(By.name("q")).click();
-    await driver.findElement(By.name("q")).sendKeys("Bangladesh", Key.RETURN);
+//   it("Search on Google a Country", async function () {
+//     await driver.manage().window().maximize();
+//     await driver.get("https://google.com");
+//     await driver.findElement(By.name("q")).click();
+//     await driver.findElement(By.name("q")).sendKeys("Bangladesh", Key.RETURN);
     
-    await driver.wait(until.elementLocated(By.id("rcnt")), 5000);
-    await driver.findElement(By.name("q")).clear();
+//     await driver.wait(until.elementLocated(By.id("rcnt")), 5000);
+//     await driver.findElement(By.name("q")).clear();
 
-    let title = await driver.getTitle();
-    assert.equal(title, "Bangladesh - Google Search");
-  });
+//     let title = await driver.getTitle();
+//     assert.equal(title, "Bangladesh - Google Search");
+//   });
 
-  it("Search on Google a city", async function () {
-    // await driver.get("https://google.com");
-    await driver.findElement(By.name("q")).click();
-    await driver.findElement(By.name("q")).sendKeys("Chattogram", Key.RETURN);
-    await driver.wait(until.elementLocated(By.id("rcnt")), 10000);
-    await driver.findElement(By.name("q")).clear();
+//   it("Search on Google a city", async function () {
+//     // await driver.get("https://google.com");
+//     await driver.findElement(By.name("q")).click();
+//     await driver.findElement(By.name("q")).sendKeys("Chattogram", Key.RETURN);
+//     await driver.wait(until.elementLocated(By.id("rcnt")), 10000);
+//     await driver.findElement(By.name("q")).clear();
 
-    let title = await driver.getTitle();
-    assert.equal(title, "Chattogram - Google Search");
-  });
+//     let title = await driver.getTitle();
+//     assert.equal(title, "Chattogram - Google Search");
+//   });
 
-  after(() => driver && driver.quit());
-});
+//   after(() => driver && driver.quit());
+// });
